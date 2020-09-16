7bf6269b9589d08ebdef3c35f4d4485024c54405
nhannvu19 <mingnhan.1996@gmail.com>
2020-09-16 15:49:14 +0700

    update

--- lib/vnpay.rb
+++ lib/vnpay.rb
@@ -1,13 +1,4 @@
 require 'order_info'
-require 'vnpay/url'
+require 'vnpay/config'
 require 'vnpay/verify'
-
-class Vnpay
-  def self.configuration
-    @configuration ||= OpenStruct.new
-  end
-
-  def self.configure
-    yield(configuration)
-  end
-end
+require 'vnpay/url'
